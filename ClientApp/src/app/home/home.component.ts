1604158404a50d6c515165e995e0e67315abbf30
Shane Duffy <shane@shaneduffy.io>
2020-07-10 15:42:10 -0700

    Added Vanta background

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const VANTA: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  vantaEffect;
+
+  ngOnInit() {
+    this.initVantaEffect();
+  }
+
+  initVantaEffect() {
+    VANTA.CLOUDS2({
+      el: "#vanta-background",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      texturePath: "assets/img/noise.png"
+    })
+  }
 }
