a2f37ab0a93ce31ab53337fd7b5f3354787ea338
Shane Duffy <shane@shaneduffy.io>
2020-09-07 16:06:11 -0700

    Changed vanta background color

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -18,14 +18,14 @@ export class HomeComponent implements OnInit {
       el: "#vanta-background",
       mouseControls: true,
       touchControls: true,
+      gyroControls: false,
       minHeight: 200.00,
       minWidth: 200.00,
       scale: 1.00,
-      color: 0xbebecf,
-      shininess: 14.00,
-      waveHeight: 30.00,
-      waveSpeed: 0.20,
-      zoom: 1.75
+      scaleMobile: 1.00,
+      color: 0x556772,
+      waveSpeed: 0.30,
+      zoom: 0.65
     })
   }
 }
